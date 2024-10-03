ae078cf18f042362ffc8c11a4db0df4281beed27
devrakibulrahman <devrakibulrahman@gmail.com>
2024-10-04 02:27:48 +0600

    api intergrate in website on 10/04/2024 at 02:27 am

--- js/app.js
+++ js/app.js
@@ -75,3 +75,78 @@ allTabBtn.forEach((tabButtons) => {
         tabBtn.classList.add('active');
     });
 });
+
+// load video api ----------------->
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then((res) => res.json())
+        .then((data) => displayVideos(data))
+        .catch((err) => console.log(err))
+};
+
+// display the videos ----------------->
+const displayVideos = (data) => {
+    const {videos} = data;
+    const vdContainer = document.querySelector('.vd-container');
+
+    videos.forEach((videoData) => {
+        const {title, thumbnail, authors, others} = videoData;
+        const {profile_picture, profile_name, verified} = authors[0];
+        const {views, posted_date} = others;
+
+        const publishedDate = (posted_date) => {
+            let totalSecond = posted_date;
+            let getHours = Math.floor(totalSecond / 3600);
+            let remainHours = getHours % 3600;
+            let getMinutes = Math.floor(remainHours / 60);
+            let getSeconds = remainHours % 60;
+            
+            return `${getHours}h ${getMinutes}m ${getSeconds}s`;
+        };
+
+        const vdCard = document.createElement('div');
+
+        vdCard.classList.add('vd-card');
+        vdCard.innerHTML = `
+            <div class="thumbnail">
+                <img src=${thumbnail} alt="">
+            </div>
+            <div class="vd-details">
+                <div class="vd-logo">
+                    <img src=${profile_picture} alt="">
+                </div>
+                <div class="vd-title-bx">
+                    <p class="vd-title">${title}.</p>
+                    <div class="vd-channel-details">
+                        <div class="vd-channel">
+                            <p class="channel-name">${profile_name}</p>
+                            <div class="verify-icon">
+                                <img src="./assets/verify.png" alt="">
+                            </div>
+                        </div>
+                        <div class="vd-views">
+                            <p class="views-title">${views}</p>
+                            <div class="dotted-divider"></div>
+                            <p class="views-title">${publishedDate(posted_date)}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+
+        vdContainer.appendChild(vdCard);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+// displayVideos()
+loadVideos()
